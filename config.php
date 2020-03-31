06b8a0facc350022ca15f8addf79e0dd00471342
YakumoRan <yys14239103@gmail.com>
2020-03-31 18:46:54 +0900

    파일옮김

--- config.php
+++ config.php
@@ -1,15 +1,15 @@
 <?php
     ini_set( "display_errors", true );
-    date_default_timezone_set( "Australia/Sydney" );  // http://www.php.net/manual/en/timezones.php
+    date_default_timezone_set( "Asia/Seoul" );  // http://www.php.net/manual/en/timezones.php
     define( "DB_DSN", "mysql:host=localhost;dbname=cms" );
-    define( "DB_USERNAME", "username" );
-    define( "DB_PASSWORD", "password" );
+    define( "DB_USERNAME", "seoil_db" );
+    define( "DB_PASSWORD", "egjScI4p1m2Al5eJ" );
     define( "CLASS_PATH", "classes" );
     define( "TEMPLATE_PATH", "templates" );
     define( "HOMEPAGE_NUM_ARTICLES", 5 );
     define( "ADMIN_USERNAME", "admin" );
-    define( "ADMIN_PASSWORD", "mypass" );
-    require( CLASS_PATH . "/Article.php" );
+    define( "ADMIN_PASSWORD", "fCyhaxvqYsR5XcP9" );
+    require( CLASS_PATH . "/article.php" );
 
     function handleException( $exception ) {
       echo "Sorry, a problem occurred. Please try later.";
