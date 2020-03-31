379accb84ef980fca2a4248b2120862e08f21c07
YakumoRan <yys14239103@gmail.com>
2020-03-31 20:24:38 +0900

    0331

--- classes/article.php
+++ classes/article.php
@@ -7,31 +7,10 @@
 class Article
 {
 
-  // Properties
-
-  /**
-  * @var int The article ID from the database
-  */
   public $id = null;
-
-  /**
-  * @var int When the article was published
-  */
-  public $publicationDate = null;
-
-  /**
-  * @var string Full title of the article
-  */
+  public $pub_date = null;
   public $title = null;
-
-  /**
-  * @var string A short summary of the article
-  */
   public $summary = null;
-
-  /**
-  * @var string The HTML content of the article
-  */
   public $content = null;
 
 
@@ -43,7 +22,7 @@ class Article
 
   public function __construct( $data=array() ) {
     if ( isset( $data['id'] ) ) $this->id = (int) $data['id'];
-    if ( isset( $data['publicationDate'] ) ) $this->publicationDate = (int) $data['publicationDate'];
+    if ( isset( $data['pub_date'] ) ) $this->pub_date = (int) $data['pub_date'];
     if ( isset( $data['title'] ) ) $this->title = preg_replace ( "/[^\.\,\-\_\'\"\@\?\!\:\$ a-zA-Z0-9()]/", "", $data['title'] );
     if ( isset( $data['summary'] ) ) $this->summary = preg_replace ( "/[^\.\,\-\_\'\"\@\?\!\:\$ a-zA-Z0-9()]/", "", $data['summary'] );
     if ( isset( $data['content'] ) ) $this->content = $data['content'];
@@ -62,12 +41,12 @@ class Article
     $this->__construct( $params );
 
     // Parse and store the publication date
-    if ( isset($params['publicationDate']) ) {
-      $publicationDate = explode ( '-', $params['publicationDate'] );
+    if ( isset($params['pub_date']) ) {
+      $pub_date = explode ( '-', $params['pub_date'] );
 
-      if ( count($publicationDate) == 3 ) {
-        list ( $y, $m, $d ) = $publicationDate;
-        $this->publicationDate = mktime ( 0, 0, 0, $m, $d, $y );
+      if ( count($pub_date) == 3 ) {
+        list ( $y, $m, $d ) = $pub_date;
+        $this->pub_date = mktime ( 0, 0, 0, $m, $d, $y );
       }
     }
   }
@@ -82,7 +61,7 @@ class Article
 
   public static function getById( $id ) {
     $conn = new PDO( DB_DSN, DB_USERNAME, DB_PASSWORD );
-    $sql = "SELECT *, UNIX_TIMESTAMP(publicationDate) AS publicationDate FROM articles WHERE id = :id";
+    $sql = "SELECT *, UNIX_TIMESTAMP(pub_date) AS pub_date FROM articles WHERE id = :id";
     $st = $conn->prepare( $sql );
     $st->bindValue( ":id", $id, PDO::PARAM_INT );
     $st->execute();
@@ -101,8 +80,8 @@ class Article
 
   public static function getList( $numRows=1000000 ) {
     $conn = new PDO( DB_DSN, DB_USERNAME, DB_PASSWORD );
-    $sql = "SELECT SQL_CALC_FOUND_ROWS *, UNIX_TIMESTAMP(publicationDate) AS publicationDate FROM articles
-            ORDER BY publicationDate DESC LIMIT :numRows";
+    $sql = "SELECT SQL_CALC_FOUND_ROWS *, UNIX_TIMESTAMP(pub_date) AS pub_date FROM articles
+            ORDER BY pub_date DESC LIMIT :numRows";
 
     $st = $conn->prepare( $sql );
     $st->bindValue( ":numRows", $numRows, PDO::PARAM_INT );
@@ -133,9 +112,9 @@ class Article
 
     // Insert the Article
     $conn = new PDO( DB_DSN, DB_USERNAME, DB_PASSWORD );
-    $sql = "INSERT INTO articles ( publicationDate, title, summary, content ) VALUES ( FROM_UNIXTIME(:publicationDate), :title, :summary, :content )";
+    $sql = "INSERT INTO articles ( pub_date, title, summary, content ) VALUES ( FROM_UNIXTIME(:pub_date), :title, :summary, :content )";
     $st = $conn->prepare ( $sql );
-    $st->bindValue( ":publicationDate", $this->publicationDate, PDO::PARAM_INT );
+    $st->bindValue( ":pub_date", $this->pub_date, PDO::PARAM_INT );
     $st->bindValue( ":title", $this->title, PDO::PARAM_STR );
     $st->bindValue( ":summary", $this->summary, PDO::PARAM_STR );
     $st->bindValue( ":content", $this->content, PDO::PARAM_STR );
@@ -156,9 +135,9 @@ class Article
    
     // Update the Article
     $conn = new PDO( DB_DSN, DB_USERNAME, DB_PASSWORD );
-    $sql = "UPDATE articles SET publicationDate=FROM_UNIXTIME(:publicationDate), title=:title, summary=:summary, content=:content WHERE id = :id";
+    $sql = "UPDATE articles SET pub_date=FROM_UNIXTIME(:pub_date), title=:title, summary=:summary, content=:content WHERE id = :id";
     $st = $conn->prepare ( $sql );
-    $st->bindValue( ":publicationDate", $this->publicationDate, PDO::PARAM_INT );
+    $st->bindValue( ":pub_date", $this->pub_date, PDO::PARAM_INT );
     $st->bindValue( ":title", $this->title, PDO::PARAM_STR );
     $st->bindValue( ":summary", $this->summary, PDO::PARAM_STR );
     $st->bindValue( ":content", $this->content, PDO::PARAM_STR );
