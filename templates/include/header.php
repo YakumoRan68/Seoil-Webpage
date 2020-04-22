8aa7710b37b4baa712d0fa14794f815a0b9ac724
YakumoRan <yys14239103@gmail.com>
2020-04-23 01:06:16 +0900

    0423

--- templates/include/header.php
+++ templates/include/header.php
@@ -6,5 +6,47 @@
     <link rel="stylesheet" type="text/css" href="css/styleMain.css" />
   </head>
   <body>
-    <div id="container">
-      <a href="."><img id="logo" src="images/logoSeoil.png" alt="서일대학교 커뮤니티 로고" /></a>
+	<header> <!--시멘틱 태그(header, footer) : 웹 페이지에서 어디가 헤더인지, 푸터인지 표시해주는 html5 표준 태그; 직접 꾸밀게 아니더라도 태그해서 표시는 해야함.-->
+		<div class="header_wrapper">
+			<a href="."><img class="logo" src="images/logoSeoil.png" alt="서일대학교 커뮤니티 로고"/></a>
+			<nav class="header_nav">
+				<ul class="nav">
+					<li><a href="#" accesskey="2" title="">커뮤니티</a>
+						<ul>
+							<li><a href="# ">자유게시판</a></li>
+							<li><a href="# ">익명게시판</a></li>
+							<li><a href="# ">새내기게시판</a></li>
+							<li><a href="# ">졸업생게시판</a></li>
+						</ul>
+					</li>
+					<li><a href="#" accesskey="3" title="">강의정보</a>
+						<ul>
+							<li><a href="# ">전공강의정보</a></li>
+							<li><a href="# ">교양강의정보</a></li>
+							<li><a href="# ">교양강의자료</a></li>
+						</ul>
+					</li>
+					<li><a href="#" accesskey="4" title="">분실물</a>
+						<ul>
+							<li><a href="# ">분실신고</a></li>
+							<li><a href="# ">습득물</a></li>
+						</ul>
+					</li>
+					<li><a href="#" accesskey="5" title="">공지사항</a>
+						<ul>
+							<li><a href="# ">학교소식</a></li>
+							<li><a href="# ">학생소식</a></li>
+							<li><a href="# ">서일동아리</a></li>
+						</ul>
+					</li>
+					<li><a href="#" accesskey="6" title="">운영센터</a>
+						<ul>
+							<li><a href="# ">업데이트 공지</a></li>
+							<li><a href="# ">게시판 신설건의</a></li>
+							<li><a href="# ">문의사항</a></li>
+						</ul>
+					</li>
+				</ul>
+			</div>
+		</div>
+	</header>
