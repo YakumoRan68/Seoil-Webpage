9d80a2d739520049641d1258d68840f66c54b9fe
YakumoRan <yys14239103@gmail.com>
2020-05-06 22:12:24 +0900

    burp

--- templates/homepage.php
+++ templates/homepage.php
@@ -7,6 +7,11 @@
 <?php include "templates/include/footer.php" ?>
 
 <!-- TODO(백엔드) 
-- url 파라메터 지우기(PHP Redirect : Location(), header())
+- 비 어드민 로그인 세션
+- 댓글
+- 파일 업로드
 - Rich Text Editor
+- url 파라메터 지우기(PHP Redirect : Location(), header())
+- 이미지 업로드
+- 데이터 보안
 -->
